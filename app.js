d7a8002b31bc1c2e4b32b054ec0d63c7609a569f
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-12 11:19:30 +0300

    Level 3 Hashing Completed

--- app.js
+++ app.js
@@ -5,18 +5,13 @@ const ejs = require("ejs");
 const exp = require("constants");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
-});
-
-userSchema.plugin(encrypt, {
-	secret: process.env.SECRET,
-	encryptedFields: ["password"],
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -40,7 +35,7 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
 	const newUser = new User({
 		email: req.body.username,
-		password: req.body.password,
+		password: md5(req.body.password),
 	});
 	const saveStatus = await newUser.save();
 	res.render("secrets");
@@ -48,7 +43,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
 	const username = req.body.username;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 	const foundUser = await User.findOne({ email: username });
 	if (foundUser) {
 		if (foundUser.password === password) {
