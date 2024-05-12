15a7a762ce28b0704529f35d788ffaa60561894d
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-12 09:31:21 +0300

    Level One Done

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const exp = require("constants");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = mongoose.Schema({
+	email: String,
+	password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+	const newUser = new User({
+		email: req.body.username,
+		password: req.body.password,
+	});
+	const saveStatus = await newUser.save();
+	res.render("secrets");
+});
+
+app.post("/login", async (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+	const foundUser = await User.findOne({ email: username });
+	if (foundUser) {
+		if (foundUser.password === password) {
+			res.render("secrets");
+		} else {
+			res.send("Error");
+		}
+	} else {
+		res.send("Error");
+	}
+});
+
+app.listen(3000, () => {
+	console.log("Server is live on port 3000");
+});
